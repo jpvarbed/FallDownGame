c254c8a5dd30d17ea2d418a2dd6ae9661c39b4c7
Trey <tgrunnagle@gmail.com>
2014-01-23 21:59:27 -0800

    draw in the canvas

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+// initialize when the DOM is loaded
+$(document).ready(function() {
+	InitializeCanvas();
+});
 
+function InitializeCanvas() {
+	// retrieve the HTML DOM object
+	var canvas = $('#mainCanvas')[0];
+	var context = canvas.getContext("2d");
+	context.rect(20, 20, canvas.width - 20, canvas.height - 20);
+	context.fillStyle = 'green';
+	context.fill();
+}
