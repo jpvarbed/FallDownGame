6a4f95393074310ed1005c2239cd33054c3530d1
Trey <tgrunnagle@gmail.com>
2015-02-10 18:37:19 -0800

    move maze block into its own file

--- site/js/main.js
+++ site/js/main.js
@@ -32,37 +32,6 @@ function StartEverything() {
 	Game.initialize();
 }
 
-function MazeBlock(x, y, h, w) {
-	this.x = x;
-	this.y = y;
-	var randHeight = Math.floor((Math.random() * h/2) + 1);
-	this.height = h + randHeight;
-	var randWidth = Math.floor((Math.random() * w/2) + 1);
-	this.width = w + randWidth;
-	this.active = true;
-	var randWeight = Math.floor((Math.random() * GRAVITY/2) + 1);
-	this.weight = GRAVITY + randWeight;
-}
-
-MazeBlock.prototype = {
-	update: function() {
-		this.y += this.weight;
-		if (this.y > MAIN_HEIGHT)
-		{
-			this.active = false;
-		}
-	},
-	draw: function() {
-		var context = canvas.getContext("2d");
-		context.fillStyle = 'blue';
-		context.fillRect(this.x, this.y, this.width, this.height);
-	}
-}
-
-MazeBlock.prototype.explode = function() {
-	this.active = false;
-}
-
 var maze = {
 
 	initalize: function()
@@ -252,11 +221,11 @@ var Game = {
 		this.drawEndGameMessage();
 		this.showRestartButton();
 		setTimeout(
-		  function()
-		  {
+			function() {
 		    	ClearCanvas();
 				StartEverything();
-		  }, 5000);
+		  	},
+		  	5000);
 	},
 	initialize: function() {
 		this.hasStarted = 0;
